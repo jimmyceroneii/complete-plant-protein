aab1b3882a35c862ce317ec15b01d620b62cab47
jimmyceroneii <jimmy.cerone@atomicobject.com>
2021-12-25 23:38:13 -0500

    added git config, configured jest, and wrote failing test

--- client/src/components/plants/plant.test.ts
+++ client/src/components/plants/plant.test.ts
@@ -1,5 +1,17 @@
+import { EssentialProtein } from "./types";
+import { removeProteinsWithPlant } from "./utils";
+
 describe("ProteinListUpdate", () => {
   it("removes an item from the list and updates the proteins", () => {
-    const initialProteinList = [];
+    const initialProteinList: EssentialProtein[] = ["Histidine"];
+
+    const plant = "bean";
+
+    const updatedProteinList = removeProteinsWithPlant(
+      plant,
+      initialProteinList,
+    );
+
+    expect(updatedProteinList.length).toEqual(0);
   });
 });
---
+++ client/src/components/plants/utils.ts
@@ -0,0 +1,13 @@
+import { EssentialProtein } from "./types";
+
+export const removeProteinsWithPlant = (
+  plantToRemove: string,
+  initialProteinList: EssentialProtein[],
+): EssentialProtein[] => {
+  // find proteins for current plant
+
+  // determine if they are unique to plant
+
+  // remove if so
+  return initialProteinList;
+};
