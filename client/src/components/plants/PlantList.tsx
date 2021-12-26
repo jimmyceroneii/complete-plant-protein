72db37f70c39617b8baf0c96daaadfb4c5308584
jimmyceroneii <jimmy.cerone@atomicobject.com>
2021-12-26 00:28:27 -0500

    WIP keeping track of proteins

--- client/src/components/plants/PlantList.tsx
+++ client/src/components/plants/PlantList.tsx
@@ -1,67 +1,99 @@
 import React, { ChangeEvent, useCallback, useState } from "react";
 import { enumToProteinMap, EssentialProtein, PlantToProtein } from "./types";
 
-const completeProteinCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-const plantToProteinMap: PlantToProtein = {
-	bean: ["Histidine"],
-	rice: ["Histidine"],
+export const plantToProteinMap: PlantToProtein = {
+  bean: ["Histidine"],
+  rice: ["Histidine"],
 };
 
 export const PlantList: React.FC = () => {
-	const allPlants = ["bean", "rice"];
-	const [checked, setChecked] = useState<string[]>(["bean"]);
-	const [proteins, setProteins] = useState<EssentialProtein[]>([]);
+  // TODO: Fix the order of operations but in that setProteins is running before setProteinCount is completed
+  const allPlants = ["bean", "rice"];
+  const [checked, setChecked] = useState<string[]>(["bean"]);
+  const [proteins, setProteins] = useState<EssentialProtein[]>(["Histidine"]);
+  const [proteinCount, setProteinCount] = useState<number[]>([
+    0, 0, 0, 0, 0, 0, 1, 0, 0,
+  ]);
 
-	const handleCheck = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-		if (event.target.checked) {
-			setChecked((x) => [...x, event.target.value]);
-			setProteins((x) => {
-				const uncleanedProteins = plantToProteinMap[event.target.value];
-				const newProteins = uncleanedProteins.filter(
-					(potentialProtein) => !x.includes(potentialProtein)
-				);
-				return [...x, ...newProteins];
-			});
-		} else {
-			setChecked((x) => {
-				const copy = x.filter((plant) => {
-					// if the protein is only on the current plant, remove it
-					return plant !== event.target.value;
-				});
-				return copy;
-			});
-			setProteins((x) => {
-				const proteinIndex = enumToProteinMap[event.target.value];
-				completeProteinCount[proteinIndex]--;
-				if (completeProteinCount[proteinIndex] === 0) {
-					const copy = proteins;
-					const updatedList = copy.filter((protein) => {
-						return protein !== event.target.value;
-					});
-					return updatedList;
-				}
-			});
-		}
-	}, []);
+  const handleCheck = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      if (event.target.checked) {
+        setChecked((x) => [...x, event.target.value]);
+        setProteinCount((x) => {
+          const proteins = plantToProteinMap[event.target.value];
 
-	return (
-		<div style={{ display: "flex", flexDirection: "column" }}>
-			{allPlants.map((plant, index) => (
-				<div key={index}>
-					<input
-						value={plant}
-						type="checkbox"
-						onChange={handleCheck}
-						checked={checked.includes(plant)}
-					/>
-					<span>{plant}</span>
-				</div>
-			))}
-			<div>
-				{proteins &&
-					proteins.map((protein, index) => <p key={index}>{protein}</p>)}
-			</div>
-		</div>
-	);
+          const copy = x;
+
+          proteins.map((protein) => {
+            const proteinIndex = enumToProteinMap[protein];
+
+            copy[proteinIndex] += 1;
+          });
+
+          return copy;
+        });
+        setProteins((x) => {
+          const uncleanedProteins = plantToProteinMap[event.target.value];
+          const newProteins = uncleanedProteins.filter(
+            (potentialProtein) => !x.includes(potentialProtein)
+          );
+          return [...x, ...newProteins];
+        });
+      } else {
+        setChecked((x) => {
+          const copy = x.filter((plant) => {
+            return plant !== event.target.value;
+          });
+          return copy;
+        });
+        setProteinCount((x) => {
+          const proteins = plantToProteinMap[event.target.value];
+
+          console.log("Proteins: ", proteins);
+
+          const copy = x;
+
+          proteins.map((protein) => {
+            const proteinIndex = enumToProteinMap[protein];
+
+            copy[proteinIndex] -= 1;
+          });
+
+          console.log("Copy: ", copy);
+
+          return copy;
+        });
+        setProteins((x) => {
+          return x.filter((protein) => {
+            const proteinIndex = enumToProteinMap[protein];
+
+            console.log("Protein Count: ", proteinCount);
+
+            return proteinCount[proteinIndex] === 0;
+          });
+        });
+      }
+    },
+    [proteinCount, proteins, checked]
+  );
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {allPlants.map((plant, index) => (
+        <div key={index}>
+          <input
+            value={plant}
+            type="checkbox"
+            onChange={handleCheck}
+            checked={checked.includes(plant)}
+          />
+          <span>{plant}</span>
+        </div>
+      ))}
+      <div>
+        {proteins &&
+          proteins.map((protein, index) => <p key={index}>{protein}</p>)}
+      </div>
+    </div>
+  );
 };
--- client/src/components/plants/plant.test.ts
+++ client/src/components/plants/plant.test.ts
@@ -1,17 +1,4 @@
-import { EssentialProtein } from "./types";
-import { removeProteinsWithPlant } from "./utils";
-
 describe("ProteinListUpdate", () => {
-  it("removes an item from the list and updates the proteins", () => {
-    const initialProteinList: EssentialProtein[] = ["Histidine"];
-
-    const plant = "bean";
-
-    const updatedProteinList = removeProteinsWithPlant(
-      plant,
-      initialProteinList,
-    );
-
-    expect(updatedProteinList.length).toEqual(0);
+  it.todo("removes an item from the list and updates the proteins", () => {
   });
 });
