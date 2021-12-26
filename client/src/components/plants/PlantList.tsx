253c618c94727f969818795b93a823966a511d82
jimmyceroneii <jimmy.cerone@atomicobject.com>
2021-12-25 21:59:58 -0500

    WIP removing proteins when plants get unchecked

--- client/src/components/plants/PlantList.tsx
+++ client/src/components/plants/PlantList.tsx
@@ -1,82 +1,67 @@
 import React, { ChangeEvent, useCallback, useState } from "react";
+import { enumToProteinMap, EssentialProtein, PlantToProtein } from "./types";
 
-type EssentialProtein = "Phenylalanine" |
-	"Valine" |
-	"Tryptophan" |
-	"Threonine" |
-	"Isoleucine" |
-	"Methionine" |
-	"Histidine" |
-	"Leucine" |
-	"Lysine";
+const completeProteinCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-type PlantToProtein = {
-	[key: string]: EssentialProtein[];
-}
-
-const completeProtein = [
-	"Phenylalanine",
-	"Valine",
-	"Tryptophan",
-	"Threonine",
-	"Isoleucine",
-	"Methionine",
-	"Histidine",
-	"Leucine",
-	"Lysine"
-]
-
-const plantToProteinMap: PlantToProtein = { "bean": ["Histidine"], "rice": ["Histidine"] }
+const plantToProteinMap: PlantToProtein = {
+	bean: ["Histidine"],
+	rice: ["Histidine"],
+};
 
 export const PlantList: React.FC = () => {
 	const allPlants = ["bean", "rice"];
-	const [checked, setChecked] = useState<string[]>(['bean']);
-	const [proteins, setProteins] = useState<EssentialProtein[]>([])
+	const [checked, setChecked] = useState<string[]>(["bean"]);
+	const [proteins, setProteins] = useState<EssentialProtein[]>([]);
 
 	const handleCheck = useCallback((event: ChangeEvent<HTMLInputElement>) => {
 		if (event.target.checked) {
 			setChecked((x) => [...x, event.target.value]);
-			console.log(plantToProteinMap);
-			console.log("Event: ", event.target.value);
 			setProteins((x) => {
 				const uncleanedProteins = plantToProteinMap[event.target.value];
-				const newProteins = uncleanedProteins.filter((potentialProtein) =>
-					!x.includes(potentialProtein)
+				const newProteins = uncleanedProteins.filter(
+					(potentialProtein) => !x.includes(potentialProtein)
 				);
 				return [...x, ...newProteins];
 			});
-			console.log(proteins);
 		} else {
 			setChecked((x) => {
 				const copy = x.filter((plant) => {
+					// if the protein is only on the current plant, remove it
 					return plant !== event.target.value;
 				});
 				return copy;
 			});
-			// remove proteins that are only from the plant unchecked
 			setProteins((x) => {
-				const copy = x.filter((plant) => {
-					return plant !== event.target.value;
-				})
-				return copy;
+				const proteinIndex = enumToProteinMap[event.target.value];
+				completeProteinCount[proteinIndex]--;
+				if (completeProteinCount[proteinIndex] === 0) {
+					const copy = proteins;
+					const updatedList = copy.filter((protein) => {
+						return protein !== event.target.value;
+					});
+					return updatedList;
+				}
 			});
 		}
-	}, [])
+	}, []);
 
 	return (
 		<div style={{ display: "flex", flexDirection: "column" }}>
 			{allPlants.map((plant, index) => (
 				<div key={index}>
-					<input value={plant} type="checkbox" onChange={handleCheck} checked={checked.includes(plant)} />
+					<input
+						value={plant}
+						type="checkbox"
+						onChange={handleCheck}
+						checked={checked.includes(plant)}
+					/>
 					<span>{plant}</span>
 				</div>
 			))}
 			<div>
-				{proteins && proteins.map((protein, index) => (
-					<p key={index}>{protein}</p>
-				)
-				)}
+				{proteins &&
+					proteins.map((protein, index) => <p key={index}>{protein}</p>)}
 			</div>
 		</div>
-	)
-}
+	);
+};
---
+++ client/src/components/plants/plant.test.ts
@@ -0,0 +1,5 @@
+describe("ProteinListUpdate", () => {
+  it("removes an item from the list and updates the proteins", () => {
+    const initialProteinList = [];
+  });
+});
